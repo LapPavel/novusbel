7e88f6b113168ac053c78493b511199cd77b7bbb
LapPavel <wizd@tut.by>
2025-03-07 12:06:42 +0300

    feat: add notfound page

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <header v-if="!$route.meta.hideHeader">
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,29 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { dirname } from 'path'
+import { resolve } from 'path'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    {
+      name: 'copy-404-plugin',
+      closeBundle() {
+        const outputPath = resolve(dirname(fileURLToPath(import.meta.url)), 'dist')
+        const indexFile = resolve(outputPath, 'index.html')
+        const errorFile = resolve(outputPath, '404.html')
+
+        fs.copyFileSync(indexFile, errorFile)
+      },
+    },
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   base: './',
